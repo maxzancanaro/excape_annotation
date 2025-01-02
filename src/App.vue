34a4339e60f317221b9c3c7365db2eddab4528fa
Massimo Zancanaro <massimo.zancanaro@gmail.com>
2025-01-02 15:07:00 +0100

    .

--- src/App.vue
+++ src/App.vue
@@ -10,21 +10,27 @@
       <v-btn icon @click="console.log('back has been pressed')">
         <v-btn icon="mdi-arrow-left"></v-btn>
       </v-btn>
-      <v-toolbar-title>Annotation</v-toolbar-title>
+      <v-toolbar-title>Annotation 1.1</v-toolbar-title>
     </v-app-bar>
+  </v-col>
+  </v-row>
 
-    <main>
+  <v-row justify="center">
+  <v-col cols="12" md="8" sm="4">
       <v-card>
-              <v-card-title>Welcome</v-card-title>
-              <v-card-text>
+        <v-card-title>Welcome</v-card-title>
+        <v-card-text>
                 This is a mobile-friendly app built with Vuetify.
-              </v-card-text>
-              <v-card-actions>
-                <v-btn color="primary">Get Started</v-btn>
-              </v-card-actions>
-            </v-card>
-    </main>
-
+        </v-card-text>
+        <v-card-actions>
+            <v-btn color="primary">Get Started</v-btn>
+        </v-card-actions>
+      </v-card>
+  </v-col>
+  </v-row>
+    
+  <v-row justify="center">
+  <v-col cols="12" md="8" sm="4">
     <v-bottom-navigation>
       <v-btn value="Participant_1">
         <span>Part. 1</span>
@@ -39,7 +45,7 @@
       </v-btn>
 
       <v-btn value="Participant_4">
-        <span>Part.</span>
+        <span>Part. 4</span>
       </v-btn>
 
     </v-bottom-navigation>
